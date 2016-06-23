b796c47f9d19e42797f99b5b5fa10e87ab2d1480
shaojx <wnxsjx@sina.com>
2016-06-23 14:34:26 +0800

    init add code

---
+++ src/com/ddas/sns/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,36 @@
+package com.ddas.sns.interceptor;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+public class LoginRequiredInterceptor implements HandlerInterceptor{
+	
+	
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse rpo,
+			Object arg2) throws Exception {
+		// TODO: 2016/6/23
+		return false;
+	}
+
+}
