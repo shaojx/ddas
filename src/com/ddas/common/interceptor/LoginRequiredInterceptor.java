f9d39b271cbdb1cff434ce8cdaac95c203a84b71
shaojx <wnxsjx@sina.com>
2016-07-06 18:05:20 +0800

    增加拦截器

--- src/com/ddas/common/interceptor/LoginRequiredInterceptor.java
+++ src/com/ddas/common/interceptor/LoginRequiredInterceptor.java
@@ -1,14 +1,20 @@
 package com.ddas.common.interceptor;
 
 
+import com.ddas.common.util.StringUtil;
+import com.ddas.sns.userinfo.domain.UserInfo;
+import com.ddas.sns.util.ReloadablePropertiesUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class LoginRequiredInterceptor implements HandlerInterceptor{
-	
+
 	
 	public void afterCompletion(HttpServletRequest request,
 			HttpServletResponse reponse, Object arg2, Exception arg3)
@@ -17,16 +23,38 @@ public class LoginRequiredInterceptor implements HandlerInterceptor{
 		
 	}
 
-	public void postHandle(HttpServletRequest request, HttpServletResponse response,
-			Object arg2, ModelAndView arg3) throws Exception {
+	public void postHandle(HttpServletRequest req, HttpServletResponse response,
+			Object handler, ModelAndView modelAndView) throws Exception {
 
-		// TODO: 2016/7/6
 	}
 
-	public boolean preHandle(HttpServletRequest req, HttpServletResponse rpo,
-			Object arg2) throws Exception {
-		// TODO: 2016/6/23
-		return false;
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse response,
+			Object handler ) throws Exception {
+       /* ReloadablePropertiesUtil reloadablePropertiesUtil=ReloadablePropertiesUtil.getInstance("resources/config.properties");
+        String excludeURLs = reloadablePropertiesUtil.getPropertyByKey("excludeURLs");
+        String requestURL=req.getRequestURL().toString();
+        String[] segs = StringUtil.split(excludeURLs, ",");
+        boolean isMatch=false;
+        for(String url:segs){
+            if(url.endsWith(requestURL)){
+                isMatch=true;
+            }
+        }*/
+       // if(!isMatch){//如果这个请求的URL不在excludeURLs中，则拦截
+            //用户登录判断
+            HttpSession session = req.getSession(true);
+            UserInfo userInfo=(UserInfo)session.getAttribute("userInfo");
+            if(userInfo==null){
+                try {
+                 //   req.getRequestDispatcher("/WEB-INF/views/login/login.jsp").forward(req,response);
+                    response.sendRedirect("/WEB-INF/views/login/login.jsp");
+                }  catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+       // }
+        return true;
 	}
 
 }
